ecaaceaaf2081359b3e8f6cd8b8090517dba73fe
DavidElPapu <david.jung@uniat.edu.mx>
2023-03-23 10:17:30 -0600

    Add askNumber function

--- GuessMyNumber.cpp
+++ GuessMyNumber.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include<cstdlib> //Trabaja con números random
+#include <string>
+
+
 using namespace std;
+
+int askNumber(string question, int high, int low = 1);
 
 int main()
 {
@@ -11,6 +16,7 @@ int main()
     int secretNumber = (randomNumber % 100) + 1;
     int guess;
     int tries = 0;
+    int veryClose;
 
     cout << "GUESS MY NUMBER" << endl;
     cout << "Adivina el número en el menor número de intentos posible." << endl;
@@ -19,10 +25,18 @@ int main()
     cout << secretNumber;
     do
     {
-        cout << "\nEnter a guess (1-100):" << endl;
-        cin >> guess;
+        
+        guess = askNumber("\nAdivina un numero ", 100);
+
         tries++;
 
+        veryClose = secretNumber - guess;
+
+
+        if (veryClose <= 5 && veryClose >= -5 && veryClose != 0)
+        {
+            cout << "Muy cerca!!\n";
+        }
         if (guess > secretNumber) {
             cout << "Muy Alto\n\n";
         }
@@ -34,4 +48,17 @@ int main()
 
         }
     } while (guess != secretNumber);
+}
+
+int askNumber(string question, int high, int low) 
+{
+
+    int number = 0;
+
+    do {
+        cout << question << "entre " << low << " y " << high << endl;
+        cin >> number;
+    } while (number > high || number < low);
+    
+    return number;
 }
